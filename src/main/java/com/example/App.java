dd9a8356736315f45405d6c50b7a886383b71c94
VERICREDHO\VHO CALLUM <vanheerdenc@verishare.co.za>
2023-01-25 12:17:20 +0200

    formatted

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,16 +1,8 @@
 package com.example;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
-import com.example.telegram.Bot;
 
 /**
  * Hello world!
@@ -18,36 +10,59 @@ import com.example.telegram.Bot;
  */
 public class App {
     static Node root;
+
     public static void main(String[] args) {
         // Create example tree
         Node root = new Node("What type of vehicle are you interested in?");
 
-Node car = new Node("What type of car are you looking for?");
-car.addChild("Sedan", new Node("What is your budget for a Sedan?")
-    .addChild("Under $20,000", new Node("The best options for you would be a Honda Civic or a Toyota Corolla. Which one would you prefer?", true, "Honda Civic or Toyota Corolla"))
-    .addChild("$20,000 to $30,000", new Node("The best options for you would be a Subaru Impreza or a Mazda3. Which one would you prefer?", true, "Subaru Impreza or Mazda3"))
-    .addChild("Over $30,000", new Node("The best options for you would be a BMW 3 Series or a Audi A4. Which one would you prefer?", true, "BMW 3 Series or Audi A4")));
-car.addChild("SUV", new Node("What is your budget for a SUV?")
-    .addChild("Under $30,000", new Node("The best options for you would be a Honda CR-V or a Toyota RAV4. Which one would you prefer?", true, "Honda CR-V or Toyota RAV4"))
-    .addChild("$30,000 to $40,000", new Node("The best options for you would be a Subaru Outback or a Ford Escape. Which one would you prefer?", true, "Subaru Outback or Ford Escape"))
-    .addChild("Over $40,000", new Node("The best options for you would be a BMW X3 or a Mercedes-Benz GLC. Which one would you prefer?", true, "BMW X3 or Mercedes-Benz GLC")));
-root.addChild("Car", car);
+        Node car = new Node("What type of car are you looking for?");
+        car.addChild("Sedan", new Node("What is your budget for a Sedan?")
+                .addChild("Under $20,000", new Node(
+                        "The best options for you would be a Honda Civic or a Toyota Corolla. Which one would you prefer?",
+                        true, "Honda Civic or Toyota Corolla"))
+                .addChild("$20,000 to $30,000", new Node(
+                        "The best options for you would be a Subaru Impreza or a Mazda3. Which one would you prefer?",
+                        true, "Subaru Impreza or Mazda3"))
+                .addChild("Over $30,000", new Node(
+                        "The best options for you would be a BMW 3 Series or a Audi A4. Which one would you prefer?",
+                        true, "BMW 3 Series or Audi A4")));
+        car.addChild("SUV", new Node("What is your budget for a SUV?")
+                .addChild("Under $30,000", new Node(
+                        "The best options for you would be a Honda CR-V or a Toyota RAV4. Which one would you prefer?",
+                        true, "Honda CR-V or Toyota RAV4"))
+                .addChild("$30,000 to $40,000", new Node(
+                        "The best options for you would be a Subaru Outback or a Ford Escape. Which one would you prefer?",
+                        true, "Subaru Outback or Ford Escape"))
+                .addChild("Over $40,000", new Node(
+                        "The best options for you would be a BMW X3 or a Mercedes-Benz GLC. Which one would you prefer?",
+                        true, "BMW X3 or Mercedes-Benz GLC")));
+        root.addChild("Car", car);
 
-Node motorcycle = new Node("What type of motorcycle are you looking for?");
-motorcycle.addChild("Cruiser", new Node("What is your budget for a Cruiser?")
-    .addChild("Under $10,000", new Node("The best options for you would be a Honda Rebel or a Yamaha V Star. Which one would you prefer?", true, "Honda Rebel or Yamaha V Star"))
-    .addChild("$10,000 to $15,000", new Node("The best options for you would be a Harley-Davidson Sportster or a Indian Scout. Which one would you prefer?", true, "Harley-Davidson Sportster or Indian Scout"))
-    .addChild("Over $15,000", new Node("The best options for you would be a Harley-Davidson Road King or a Indian Chieftain. Which one would you prefer?", true, "Harley-Davidson Road King or Indian Chieftain")));
-motorcycle.addChild("Sport", new Node("What is your budget for a Sport motorcycle?")
-    .addChild("Under $8,000", new Node("The best options for you would be a Kawasaki Ninja or a Yamaha R3. Which one would you prefer?", true, "Kawasaki Ninja or Yamaha R3"))
-    .addChild("$8,000 to $12,000", new Node("The best options for you would be a Suzuki GSX-R or a Honda CBR. Which one would you prefer?", true, "Suzuki GSX-R or Honda CBR")));
-root.addChild("Motorcycle", motorcycle);
-      
-    DecisionTree decisionTree = new DecisionTree(root);
-    Scanner input = new Scanner(System.in);
-    String answer = decisionTree.evaluate(input);
-    System.out.println("Based on your responses, we suggest: " + answer);
-    input.close();
+        Node motorcycle = new Node("What type of motorcycle are you looking for?");
+        motorcycle.addChild("Cruiser", new Node("What is your budget for a Cruiser?")
+                .addChild("Under $10,000", new Node(
+                        "The best options for you would be a Honda Rebel or a Yamaha V Star. Which one would you prefer?",
+                        true, "Honda Rebel or Yamaha V Star"))
+                .addChild("$10,000 to $15,000", new Node(
+                        "The best options for you would be a Harley-Davidson Sportster or a Indian Scout. Which one would you prefer?",
+                        true, "Harley-Davidson Sportster or Indian Scout"))
+                .addChild("Over $15,000", new Node(
+                        "The best options for you would be a Harley-Davidson Road King or a Indian Chieftain. Which one would you prefer?",
+                        true, "Harley-Davidson Road King or Indian Chieftain")));
+        motorcycle.addChild("Sport", new Node("What is your budget for a Sport motorcycle?")
+                .addChild("Under $8,000", new Node(
+                        "The best options for you would be a Kawasaki Ninja or a Yamaha R3. Which one would you prefer?",
+                        true, "Kawasaki Ninja or Yamaha R3"))
+                .addChild("$8,000 to $12,000", new Node(
+                        "The best options for you would be a Suzuki GSX-R or a Honda CBR. Which one would you prefer?",
+                        true, "Suzuki GSX-R or Honda CBR")));
+        root.addChild("Motorcycle", motorcycle);
+
+        DecisionTree decisionTree = new DecisionTree(root);
+        Scanner input = new Scanner(System.in);
+        String answer = decisionTree.evaluate(input);
+        System.out.println("Based on your responses, we suggest: " + answer);
+        input.close();
     }
 }
 
@@ -63,6 +78,7 @@ class Node {
         this.isLeaf = isLeaf;
         this.answer = answer;
     }
+
     public Node(String question) {
         this.question = question;
         this.children = new HashMap<String, Node>();
